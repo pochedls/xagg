4b89501655c23152524cd5c188b4c39d8be24c8a
Paul J. Durack <durack1@llnl.gov>
2020-06-18 10:10:20 -0700

    testing CMIP6

--- tools/organize_data.py
+++ tools/organize_data.py
@@ -74,7 +74,10 @@ for fn in files:
         fs = [base, mip_era, output, institute, model, experiment, frequency,
               realm, table, rip, version, variable]
 
-    dirOut = os.path.join(fs) + '/'
+    print('fs:',fs)
+    dirOut = os.path.join(*fs)
+    print('dirOut:',dirOut)
+    #+ '/'
     f.close()
     if testMode:
         print(fn, dirOut + fn.split('/')[-1])
@@ -82,10 +85,13 @@ for fn in files:
         fx.ensure_dir(dirOut)
         # Copy file stats
         fileStat = os.stat(fn)
+        print('file stat fn:',fn,fileStat.st_atime,fileStat.st_ctime,fileStat.st_mtime)
         fo = dirOut + fn.split('/')[-1]
         os.rename(fn, fo)
         # Apply stats back to new file
         os.utime(fo, (fileStat.st_atime, fileStat.st_mtime))
+        fileStat = os.stat(fo)
+        print('file stat fo:',fo,fileStat.st_atime,fileStat.st_ctime,fileStat.st_mtime)
 
 if not testMode:
     sio.savemat('fileArchive.mat', {'fileArchive': fileArchive})
